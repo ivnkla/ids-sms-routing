dd7a4c6bc168384b1633ccffaf1e8bf22c3097f7
Kalvin-Giang Pichon <pichonk@gmail.com>
2026-04-13 17:40:23 +0200

    docs: comment method

--- src/AntenneInterface.java
+++ src/AntenneInterface.java
@@ -1,27 +1,25 @@
 import java.awt.*;
 
 public interface AntenneInterface {
-	
-
-
 	/*
 	 * An antenna can send message over the network.
 	 * It does not know to who it is sent.
 	 * The endpoint of the send communication is 
 	 * actually defined at configuration.
+	 * 
 	 * If the destination of the message is not this
      * antenna, then relay to all of the accessible 
      * antenna
 	 * 
-	 * It just take a packet in argument and
-	 * send it over the network.
+	 * All in all just take a packet in argument and
+	 * send it over its neighbor list
 	 */
 	void broadcastMessage(Packet p);
 
     /*
-     * Idle method that listen on its receiving socket.
-     * The receive socket is as the send one defined at
-     * configuration.
+     * Method that launch all the outgoing
+     * sockets. After that, each listening sockets
+     * is well mapped with one thread.
      */
     void initCommunication();
 }
