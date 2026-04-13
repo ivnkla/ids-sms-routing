01aa0d3ff0ceef048ce09bef8786f5e5bbceeb66
Kalvin-Giang Pichon <pichonk@gmail.com>
2026-04-13 20:25:40 +0200

    refacto: rmq queues -> rmq exchanges

--- src/AntenneImplementation.java
+++ src/AntenneImplementation.java
@@ -7,7 +7,6 @@ import com.rabbitmq.client.DeliverCallback;
 import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -15,30 +14,28 @@ import java.util.concurrent.TimeoutException;
 
 
 public class AntenneImplementation implements AntenneInterface {
-	
+
 	/*
 	 * Define the position of the antenna in the space
 	 */
     private Integer x;
     private Integer y;
     private Integer r;
-    
+
 	private Integer id;
-	
+
 	/*
 	 * A matrix explicit which antennas can communicate
 	 * in line and columns we have a, b, c, ...
 	 * Matrix has true if the communication can be done
 	 * false otherwise
-	 * 
+	 *
 	 * How it is filled ?
 	 * See later
 	 */
     private Boolean[][] matrice;
 	private ArrayList<Integer> neighbours;
-	private ArrayList<String> inQueues;
-	private ArrayList<String> outQueues;
-	
+
 	/*
 	 * RabbitMQ attributes
 	 */
@@ -52,7 +49,7 @@ public class AntenneImplementation implements AntenneInterface {
 
 
     public AntenneImplementation(Integer x, Integer y, Integer r, Integer idAntenne, Boolean[][] matNeighbours) throws IOException, TimeoutException {
-		
+
     	/*
     	 * Fill the basic attributes
     	 */
@@ -61,25 +58,16 @@ public class AntenneImplementation implements AntenneInterface {
 		this.r = r;
     	this.id = idAntenne;
     	this.matrice = matNeighbours;
-    	
-    	/*
-    	 * Fill the neighbors and queues lists
-    	 */
-    	
+
     	neighbours = new ArrayList<Integer>();
-    	inQueues = new ArrayList<String>();
-    	outQueues = new ArrayList<String>();
-    	
 		generateNeighbours();
-		generateInQueues();
-		generateOutQueues();
-		
+
 		/*
-		 * Activate the queues in RabbitMQ
+		 * Connect to RabbitMQ
 		 */
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost("localhost");
-		
+
 		try {
 			connection = factory.newConnection();
 			channel = connection.createChannel();
@@ -87,42 +75,37 @@ public class AntenneImplementation implements AntenneInterface {
 			System.err.println("[!] Impossible de se connecter à RabbitMQ : " + e.getMessage());
 			throw e;
 		}
-			
-			String queue = null;
-			boolean durable = false; // no survive a server restart
-			boolean exclusive = false; // do we restricted the queue to this connection ? We do not care
-			boolean autoDelete = false; // auto-delete queue (server will delete it when no longer in use)
-			Map<String, Object> arguments = null;
-			
-			/*
-			 * Declare all queues
-			 */
-			for (String q : inQueues) {
-				queue = q;
-				channel.queueDeclare(queue, durable, exclusive, autoDelete, arguments);
-			}
-			
-			for (String q : outQueues) {
-				queue = q;
-				channel.queueDeclare(queue, durable, exclusive, autoDelete, arguments);
-			}
-			
-			initCommunication();
+
+		boolean durable = false;    // no survive a server restart
+		boolean exclusive = false;  // do we restricted the queue to this connection ? We do not care
+		boolean autoDelete = false; // auto-delete queue (server will delete it when no longer in use)
+
+		/*
+		 * Declare this antenna's fanout exchange and its personal queue
+		 */
+		channel.exchangeDeclare("ex-" + this.id, "fanout");
+		channel.queueDeclare("queue-" + this.id, durable, exclusive, autoDelete, null);
+
+		/*
+		 * Bind this antenna's queue to each neighbour's exchange
+		 * so that when a neighbour broadcasts, we receive it
+		 */
+		for (int n : neighbours) {
+			channel.exchangeDeclare("ex-" + n, "fanout");
+			channel.queueBind("queue-" + this.id, "ex-" + n, "");
+		}
+
+		initCommunication();
     }
 
 	@Override
 	public void initCommunication() {
-		// TODO Auto-generated method stub
-		// TODO Factorise function
-		/*
-		 * Callback that handle the packet
-		 */
 		DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false); // not sure of that
+            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
 			Packet p;
 			try {
 				p = Packet.deserialize(delivery.getBody());
-				
+
 				/*
 				 * First check if the message has already been seen once
 				 */
@@ -131,7 +114,7 @@ public class AntenneImplementation implements AntenneInterface {
 					System.out.println("[~] Duplicate dropped : " + msgKey);
 					return;
 				}
-				
+
 				/*
 				 * Then checks the own of the message
 				 */
@@ -139,44 +122,35 @@ public class AntenneImplementation implements AntenneInterface {
 					System.out.println("[x] Received : " + p);
 				} else {
 					broadcastMessage(p);
-					System.out.println("[-] Relaying ");
+					System.out.println("[-] Relaying " + p);
 				}
 			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
         };
-        
+
 		/*
-		 * Create a single listening thread for each inQueue
+		 * Single listening thread on this antenna's queue
 		 */
-		boolean autoAck = false;
-		for (String inQueue : inQueues) {
-		    Thread listener = new Thread(() -> {
-		        try {
-		            channel.basicConsume(inQueue, autoAck, deliverCallback, consumertag -> {});
-		        } catch (IOException e) {
-		            e.printStackTrace();
-		        }
-		    });
-		    listener.setName("listener-" + inQueue);
-		    listener.setDaemon(true);
-		    listener.start();
-		}
+		Thread listener = new Thread(() -> {
+		    try {
+		        channel.basicConsume("queue-" + this.id, false, deliverCallback, consumertag -> {});
+		    } catch (IOException e) {
+		        e.printStackTrace();
+		    }
+		});
+		listener.setName("listener-" + this.id);
+		listener.setDaemon(true);
+		listener.start();
 	}
 
+	@Override
 	public void broadcastMessage(Packet p) {
-		int from = p.getFrom();
-		String QUEUE_FROM_OUT = "queue-" + this.id + "-" + from;
-		for (String outQueue : outQueues) {
-			if (!outQueue.equals(QUEUE_FROM_OUT)) {
-				try {
-					channel.basicPublish("", outQueue, null, Packet.serialize(p));
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+		seenMessages.add(p.getFrom() + "-" + p.getMsgId());
+		try {
+			channel.basicPublish("ex-" + this.id, "", null, Packet.serialize(p)); // we publish onto the exchange, rmq does the job for us
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -190,31 +164,7 @@ public class AntenneImplementation implements AntenneInterface {
 				neighbours.add(i);
 		}
 	}
-	
-	/*
-	 * Generate all the in queues that point to the antenna.
-	 */
-	private void generateInQueues() {
-		String QUEUE_NAME_SUBSCRIBE;
-		for (Integer n : neighbours) {
-			// generate queue of the form "queue-2-1" if 1 and 2 can communicate
-			QUEUE_NAME_SUBSCRIBE = "queue-" + n + "-" + Integer.toString(id);
-			inQueues.add(QUEUE_NAME_SUBSCRIBE);
-		}
-	}
-	
-	/*
-	 * Generate all the queues that point out of the antenna
-	 */
-	private void generateOutQueues() {
-		String QUEUE_NAME_PUBLISH;
-		for (Integer n : neighbours) {
-			// generate queue of the form "queue-1-2" if 1 and 2 can communicate
-			QUEUE_NAME_PUBLISH = "queue-" + Integer.toString(id) + "-" + n;
-			outQueues.add(QUEUE_NAME_PUBLISH);
-		}
-	}
-	
+
 	public static void main(String[] argv) throws Exception {
 		int id = Integer.parseInt(argv[0]);
 		String configFile = argv[1];
@@ -241,7 +191,7 @@ public class AntenneImplementation implements AntenneInterface {
 			if (parts.length < 2) {
 				switch (parts[0]){
 					case "quit":
-						System.out.println("Destructionn de l'antenne");
+						System.out.println("Destruction de l'antenne");
 						System.exit(0);
 				}
 
@@ -249,7 +199,12 @@ public class AntenneImplementation implements AntenneInterface {
 				continue;
 			}
 			int to = Integer.parseInt(parts[0]);
-			antenne.broadcastMessage(new Packet(id, to, parts[1], msgId++));
+			Packet p = new Packet(id, to, parts[1], msgId++);
+			if (to == id) {
+				System.out.println("[x] Received : " + p);
+			} else {
+				antenne.broadcastMessage(p);
+			}
 		}
     }
 }
